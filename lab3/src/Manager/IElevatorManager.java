5d35dc07204703cf951c829f11f778a86d388dc4
Enikeev00 <65538891+wefcwsdfwe@users.noreply.github.com>
2024-12-16 15:15:00 +0600

    Delete lab3 directory

--- lab3/src/Manager/IElevatorManager.java
+++
@@ -1,8 +0,0 @@
-package Manager;
-
-import java.util.List;
-
-public interface IElevatorManager {
-    void Run(List<int[]> queueCalls);
-    
-}
--- lab3/src/Maps/IElevatorMap.java
+++
@@ -1,7 +0,0 @@
-package Maps;
-
-import State.IState;
-
-public interface IElevatorMap {
-    IState GetVerifiedState(int currentFloor, IState currentState);
-}
